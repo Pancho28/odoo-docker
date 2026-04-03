6461a1d6b118c58a983f7fefad1cba74e4c469d7
Francisco Bravo <49881810+Pancho28@users.noreply.github.com>
2026-04-03 16:39:47 -0400

    Fix entrypoint execution command

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ chown -R odoo:odoo /var/lib/odoo /mnt/extra-addons /debug
 
 echo "🚀 [Railway] Permisos listos. Pasando el mando al script oficial de Odoo..."
 
-exec /entrypoint.sh "$@"
+exec /entrypoint.sh "$@" 
