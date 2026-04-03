eeea956aacfeb061fe45903a5cfbea82219e1cdd
Francisco Bravo <49881810+Pancho28@users.noreply.github.com>
2026-04-03 16:21:44 -0400

    Fix command execution in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ chown -R odoo:odoo /var/lib/odoo /mnt/extra-addons /debug
 # 2. Ejecutamos el comando original (odoo) usando gosu
 # Esto cambia el proceso al usuario 'odoo' manteniendo el PID 1
 echo "Iniciando Odoo 19 con el usuario odoo..."
-exec gosu odoo odoo "$@"
+exec gosu odoo "$@"
