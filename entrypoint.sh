fbb4a64bfe7fb6d2665bcf9cb4e84cc8c16df612
Francisco Bravo <49881810+Pancho28@users.noreply.github.com>
2026-04-03 16:38:24 -0400

    Update entrypoint.sh for Railway permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-# 1. Corregimos permisos para Railway (lo que nos dio el error 500 antes)
-echo "🔧 Ajustando permisos para Railway..."
+echo "🔧 [Railway] Ajustando permisos de carpetas..."
 chown -R odoo:odoo /var/lib/odoo /mnt/extra-addons /debug
 
-# 2. LLAMAMOS AL SCRIPT OFICIAL DE ODOO
-echo "🚀 Pasando el mando al inicializador oficial de Odoo..."
+echo "🚀 [Railway] Permisos listos. Pasando el mando al script oficial de Odoo..."
+
 exec /entrypoint.sh "$@"
